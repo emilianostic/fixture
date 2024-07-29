308419e7f03f16121f9dac0b716d0c47dc63bc74
emilianostic <emilianostic@gmail.com>
2024-07-29 16:40:35 -0300

    agrego home al extremo superir izquierdo

---
+++ componentes/Imagen1.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Image } from "react-native";
+import styled from "styled-components/native";
+
+const Imagen1 = () => {
+  const ImageMenu = styled(Image)`
+    width: 50px;
+    height: 50px;
+  `;
+  const Container = styled(View)`
+    flex: 1;
+    justify-content: flex-start;
+    align-items: flex-start;
+  `;
+
+  return (
+    <Container>
+      <ImageMenu source={require("../assets/home.png")}></ImageMenu>
+    </Container>
+  );
+};
+export default Imagen1;
